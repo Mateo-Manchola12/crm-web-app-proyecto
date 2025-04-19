201afaa5cbfa744a643c70cd01c276d3ad986f12
Mateo Manchola <mmanchola12temporal@gmail.com>
2025-04-19 18:15:36 +0200

    Refactorizar la estructura de las variables de entorno para mejorar optimizacion

--- server/src/constants/db.ts
+++ server/src/constants/db.ts
@@ -1,7 +1,9 @@
-export const DB_HOST = process.env.PGHOST as string
-export const DB_PORT = Number(process.env.PGPORT) as number
-export const DB_USER = process.env.PGUSER as string
-export const DB_PASSWORD = process.env.PGPASSWORD
+const { PGHOST, PGPORT, PGUSER, PGPASSWORD, PGSESSIONS, PGAPP } = process.env
 
-export const DB_SESSIONS_NAME = process.env.PGSESSIONS as string
-export const DB_APP_NAME = process.env.PGAPP as string
+export const DB_HOST = PGHOST as string
+export const DB_PORT = Number(PGPORT) as number
+export const DB_USER = PGUSER as string
+export const DB_PASSWORD = PGPASSWORD
+
+export const DB_SESSIONS_NAME = PGSESSIONS as string
+export const DB_APP_NAME = PGAPP as string
